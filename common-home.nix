bdc44874fe3a5bb6c0eca01713614b5586a8fc3a
Amogh Rathore <amoghr@amazon.com>
2026-05-15 19:30:05 +0000

    Add mise to PATH

--- common-home.nix
+++ common-home.nix
@@ -95,6 +95,9 @@
         autoload -U edit-command-line
         zle -N edit-command-line
         bindkey '^Xe' edit-command-line
+
+        # Mise
+        export PATH=$HOME/.local/share/mise/shims:$PATH
       '';
     autosuggestion.enable = true;
     defaultKeymap = "emacs";
